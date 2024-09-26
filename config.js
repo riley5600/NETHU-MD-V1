794730789910013fc9808745aa23631b14eb9f2f
riley5600 <160339886+riley5600@users.noreply.github.com>
2024-09-26 15:48:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://imgtr.ee/images/2024/09/17/37f9be86
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm alive now",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-SUDO_NB: process.env.SUDO_NB || "94704227534",
-OWNER_NAME: process.env.OWNER_NAME || "Nethmika MAIN",
-BOT_NAME: process.env.BOT_NAME || "NETHU-MD",
+SUDO_NB: process.env.SUDO_NB || "94725717180",
+OWNER_NAME: process.env.OWNER_NAME || "Shadeyy MAIN",
+BOT_NAME: process.env.BOT_NAME || "SHADEYY-MD",
 AUTO_VOICE: process.env.AUTO_VOICE || "true"
 
 };
